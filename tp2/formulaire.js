de6534db638ba9f9e5becb1d8f63d892b96303b5
chaibsara <chaib-sara@outlook.com>
2016-11-07 08:57:17 +0100

    Add files via upload

---
+++ tp2/formulaire.js
@@ -0,0 +1,18 @@
+//document.getElementById('error)
+
+function validation() {
+	// body...
+
+	var nom=document.getElementById('Nom').value;
+		if (nom.length < 5 ){
+
+		alert("le Nom doit conntenir au minimum 5 caractères");
+	}
+	
+else {
+	alert("Bienvenue" );
+}
+	
+	
+
+}
